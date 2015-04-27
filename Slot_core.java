98e1936dc53cc859c1b694f764e39ff7fd47de88
naka <e115756@gmail.com>
2015-04-27 15:05:58 +0900

    Create Slot_core.java
    Add Slot_core.java
    it is main function of Slot.

---
+++ Slot_core.java
@@ -0,0 +1,29 @@
+package slot;
+
+public class Slot_core {
+	
+	int[] buttom = new int[10];
+	buttom[0] = 1;
+	buttom[1] = 2;
+	buttom[2] = 3;
+
+
+
+	public void getInput(String input){
+		
+
+
+
+        int inputInt = Integer.parseInt(input);
+
+        if(buttom[inputInt - 1] == inputInt){
+        	buttom[inputInt - 1] = 0;
+        } else {
+        	System.out.println("push buttom!!(1 or 2 or 3");
+        }
+
+
+
+	}
+
+}
